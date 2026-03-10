a8c356abfbad8545c5b25db9486ab724a616147d
Felipe Viana <felipe.viana@spassu.com.br>
2026-03-10 16:06:23 -0300

    Exibir informacoes dos eventos e historico de email

--- wp-content/themes/intranet/classes/HistoricoParticipacoes/template-parts/lista-eventos.php
+++ wp-content/themes/intranet/classes/HistoricoParticipacoes/template-parts/lista-eventos.php
@@ -1,10 +1,19 @@
-<?php extract( $args ); ?>
+<?php 
+    extract( $args );
+    $agora = new \DateTime('now', new DateTimeZone('America/Sao_Paulo'));
+?>
 
 <?php if ( !$eventos ) : ?>
     <div id='lista-envios'>
         <h6 class='p-5 text-center'>Para visualizar o histórico dos participantes, informe o CPF.</h6>
     </div>
 <?php endif; ?>
+
+<?php
+    //echo "<pre>";
+    //print_r($eventos);
+    //echo "</pre>";
+?>
 
 <?php if ( $eventos ) : ?>
     <div class="form-group mt-3 filtro-eventos-participante">
@@ -44,18 +53,177 @@
         <tbody>
             <?php foreach ( $eventos as $evento ): ?>
                 <tr data-inscricao="<?php echo esc_html( $evento->id ); ?>" data-tipo="<?php echo esc_html( $evento->tipo ); ?>">
-                    <td><?php echo esc_html( $evento->id ); ?></td>
+                    <td><?php echo esc_html( $evento->post_id ); ?></td>
                     <td class="nome-evento">
                         <?php echo esc_html( $evento->nome_evento ); ?><br>
                         <span class="badge badge-primary px-2"><?php echo esc_html( mb_strtoupper( $evento->tipo ) ); ?></span>
                     </td>
-                    <td><?= $evento->sorteado ? 'Sim' : 'Não' ?></td>
-                    <td><?= $evento->confirmou_presenca ? 'Sim' : 'Não' ?></td>
-                    <td><?php echo $evento->enviou_email_instrucoes ? 'Sim <i class="fa fa-eye fa-lg ver-email-instrucao" aria-hidden="true"></i>' : 'Não'; ?></td>
-                    <td><?= esc_html( $evento->contato ?? '-' ) ?></td>
-                    <td><?= $evento->compareceu ? 'Sim' : 'Não' ?></td>
+                    <td>
+                        <?php
+                            if($evento->tipo == 'sorteio') {
+                                echo $evento->sorteado ? 'Sim' : 'Não';
+                            } else {
+                                echo 'N/A';
+                            }
+                        ?>
+                    </td>
+                    <td>
+                        <?php 
+                            if( $evento->prazo_confirmacao && ($evento->sorteado || $evento->tipo == 'cortesia') ) {
+                                $confirmacaoPresenca = $evento->confirmou_presenca;
+                                $data_validar = new DateTime($evento->prazo_confirmacao, new DateTimeZone('America/Sao_Paulo'));
+                                
+                                if($confirmacaoPresenca == '1'){
+                                    echo '<span class="dest-azul">SIM</span>';
+                                } elseif($confirmacaoPresenca == '2'){
+                                    echo '<span class="dest-azul">NÃO, CANCELOU</span>';
+                                } else {			
+                                    if($agora > $data_validar){
+                                        echo '<span class="dest-vermelho">PRAZO EXPIRADO</span>';
+                                    } else {
+                                        echo '<span class="dest-azul">AINDA NÃO RESPONDEU</span>';
+                                    }
+                                }
+                            } else {
+                                echo '-';
+                            }
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                            if($evento->sorteado || $evento->tipo == 'cortesia') {
+                                if($evento->enviou_email_instrucoes) {
+                                    if($evento->tem_historico) {
+                                        echo 'Sim <button data-toggle="tooltip" data-placement="right" title="Ver instruções" class="ver-email-instrucao btn btn-sm btn-link" data-inscricao="'.$evento->id.'"><i class="fa fa-eye fa-lg"></i></button>';
+                                    } else {
+                                        echo '<span data-toggle="tooltip" data-placement="right" title="Sem registro no histórico">
+                                                Sim 
+                                                <button class="btn btn-sm btn-link" disabled>
+                                                    <i class="fa fa-eye-slash fa-lg" aria-hidden="true"></i>
+                                                </button>
+                                            </span>';
+                                    }
+                                } else {
+                                    echo '<span data-toggle="tooltip" data-placement="right" title="Instruções pendentes">Não ⚠️</span>';
+                                }
+                            } else {
+                                echo '-';
+                            }
+                        ?>
+                        </td>
+                    <td>
+                        <?php 
+                            if($evento->sorteado || $evento->tipo == 'cortesia') {
+                                switch ($evento->tipo_contato) {
+                                    case '1':
+                                        echo '<span data-togle="tooltip" data-placement="right" title="Contato por ligação"><img src="' . get_template_directory_uri() . '/img/icon-telefone.svg" alt="icone Telefone"></span>';
+                                        break;
+                                    case '2':
+                                        echo '<span data-toggle="tooltip" data-placement="right" title="Contato por E-mail"><img src="' . get_template_directory_uri() . '/img/icon-email.svg" alt="icone Email"></span>';
+                                        break;
+                                    case '3':
+                                        echo '<span data-toggle="tooltip" data-placement="right" title="Contato por WhatsApp"><img src="' . get_template_directory_uri() . '/img/icon-whatsapp.svg" alt="icone Whatsapp"></span>';
+                                        break;
+                                    default:
+                                        echo '-';
+                                }
+                            } else {
+                                echo '-';
+                            }
+                        ?>
+                    </td>
+                    <td><?php
+                        if($evento->sorteado || $evento->tipo == 'cortesia') {
+                            echo $evento->compareceu ? 'Sim' : '<span data-toggle="tooltip" data-placement="right" title="Nesta notícia foi registrada a sanção por ausência ou resgate do prêmio.">Não ⛔</span>';
+                        } else {
+                            echo '-';
+                        }
+                        ?>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
 <?php endif; ?>
+
+<div class="modal fade" id="modalEmailInstrucao" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title">
+                    Detalhes do envio do e-mail
+                </h5>
+
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+
+                <div class="container-fluid">
+
+                    <div class="row mb-3">
+
+                        <div class="col-md-3">
+                            <h6><strong>Participante</strong></h6>
+
+                            <p id="email-participante-nome" class="mb-0"></p>
+                            <p id="email-participante-email1" class="mb-0"></p>
+                            <p id="email-participante-email2" class="mb-0"></p>
+                        </div>
+
+                        <div class="col-md-3">
+                            <h6><strong>Evento</strong></h6>
+
+                            <p id="email-evento" class="mb-0"></p>
+                        </div>
+
+                        <div class="col-md-3">
+                            <h6><strong>Administrador responsável</strong></h6>
+
+                            <p id="email-admin" class="mb-0"></p>
+                        </div>
+
+                        <div class="col-md-3">
+                            <h6><strong>Data/Hora</strong></h6>
+
+                            <p id="email-data" class="mb-0"></p>
+                        </div>
+
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+
+                            <h6>
+                                <strong>Descrição do e-mail de instrução</strong>
+                            </h6>
+
+                            <div id="email-mensagem" class="border p-3 bg-light"></div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="modal-footer">
+
+                <button class="btn btn-outline-warning" id="copiar-dados-email">
+                    <i class="fa fa-clone" aria-hidden="true"></i> Copiar dados
+                </button>
+
+                <button class="btn btn-laranja" data-dismiss="modal">
+                    Fechar
+                </button>
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
