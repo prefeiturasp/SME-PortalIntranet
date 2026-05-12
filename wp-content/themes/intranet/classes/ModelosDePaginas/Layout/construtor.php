b00159e7d7634d396d438fd78949383dd0a3e2db
Felipe Viana <felipe.viana@spassu.com.br>
2026-05-12 11:08:21 -0300

    Criar aba Banco de Talentos na pagina Meu Curriculo

--- wp-content/themes/intranet/classes/ModelosDePaginas/Layout/construtor.php
+++ wp-content/themes/intranet/classes/ModelosDePaginas/Layout/construtor.php
@@ -424,13 +424,21 @@ class Construtor extends Util
 											elseif( get_row_layout() == 'filtro_sorteios' ):
 												get_template_part( 'construtor/construtor', 'filtro_sorteios' );
 
+											// Menu de abas
+											elseif( get_row_layout() == 'menu_de_abas' ):
+												get_template_part( 'construtor/construtor', 'menu_de_abas' );
+
+											// Meu curriculo
+											elseif( get_row_layout() == 'meu_curriculo' ):
+												get_template_part( 'construtor/construtor', 'meu_curriculo' );
+
 											// Carrossel de sorteios e cortesias em destaque
 											elseif( get_row_layout() == 'fx_cl1_destaques_sorteios_cortesias_1_1' ):
 												get_template_part( 'construtor/construtor', 'sorteios_cortesias_destaques' );
 
 											// Links de acesso rápido
 											elseif( get_row_layout() == 'fx_cl1_links_acesso_rapido_1_1' ):
-												get_template_part( 'construtor/construtor', 'links_acesso_rapido_1_1' );
+												get_template_part( 'construtor/construtor', 'links_acesso_rapido_1_1' );											
 
 											// Integracao Pagina
 											elseif( get_row_layout() == 'integrar_pagina' ):
